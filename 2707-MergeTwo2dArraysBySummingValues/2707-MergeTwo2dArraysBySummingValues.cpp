7b3cad1bb832b5a62d34f12cd93c54a17fa4761f
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2025-09-16 00:52:18 +0530

    Create 2707-MergeTwo2dArraysBySummingValues

---
+++ 2707-MergeTwo2dArraysBySummingValues/2707-MergeTwo2dArraysBySummingValues.cpp
@@ -0,0 +1,39 @@
+// Last updated: 9/16/2025, 12:52:18 AM
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        vector<vector<int>> result;
+        int i = 0 ; int j = 0;
+        while(i < nums1.size() && j <nums2.size())
+        {
+            if (nums1[i][0] == nums2[j][0])
+            {
+                result.push_back({nums1[i][0],nums1[i][1] + nums2[j][1]});
+                i ++; j++;
+            }
+            else if (nums1[i][0]< nums2[j][0])
+            {
+                result.push_back({nums1[i][0],nums1[i][1]});
+                i++; 
+            }
+            else 
+            {
+                result.push_back({nums2[j][0],nums2[j][1]});
+                j++;
+            }
+        }
+        while (i <nums1.size())
+        {
+            result.push_back({nums1[i][0],nums1[i][1]});
+            i++;
+        }
+
+        while (j <nums2.size())
+        {
+            result.push_back({nums2[j][0],nums2[j][1]});
+            j++;
+        }
+
+        return result;
+    }
+};
