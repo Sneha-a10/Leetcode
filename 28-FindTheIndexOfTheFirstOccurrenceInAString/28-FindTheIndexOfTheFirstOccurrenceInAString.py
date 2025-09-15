f427f84189a0c1c4509919fbda22c69c6a3eacb7
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2025-09-16 00:53:32 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.py
@@ -0,0 +1,10 @@
+# Last updated: 9/16/2025, 12:53:33 AM
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
+        
