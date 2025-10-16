472de0c0191415fa47385e62ba9ba9fcf2d287e6
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2025-10-16 18:32:24 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/version/cpp/242-ValidAnagram_20251016_183223.cpp
@@ -0,0 +1,17 @@
+// Last updated: 10/16/2025, 6:32:23 PM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> ss;
+        unordered_map<char,int> tt;
+
+        for(auto s1 : s) ss[s1]++;
+        for(auto t1 : t) tt[t1]++;
+
+        // for(auto it : ss){
+        //     if(ss[it.first] != tt[it.first]) return false;
+        // }
+        if(ss == tt) return true;
+        return false;
+    }
+};
