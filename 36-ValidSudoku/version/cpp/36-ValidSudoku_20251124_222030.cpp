b5f3d2e38d8983781560bc240660b48d6268f990
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2025-11-24 22:20:32 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/version/cpp/36-ValidSudoku_20251124_222030.cpp
@@ -0,0 +1,32 @@
+// Last updated: 11/24/2025, 10:20:30 PM
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) 
+    {
+        for(int i = 0; i<9; i++)
+        {
+            for(int j = 0; j<9; j++)
+                if(board[i][j]!= '.')
+                {
+                    int cnt = 0;
+
+                    for(int k = 0; k<9; k++)
+                    {
+                        if(board[i][j] == board[i][k]) cnt++;
+                        if(board[i][j] == board[k][j]) cnt++;
+                    }
+
+                    int x = (i/3) *3;
+                    int y = (j/3) *3;
+
+                    for(int k = x; k < x+3; k++)
+                        for(int l = y; l < y+3; l++)
+                            if(board[i][j] == board[k][l]) cnt++;
+                    
+                    if (cnt > 3) return false;
+                }
+        }
+
+        return true;
+    }
+};
