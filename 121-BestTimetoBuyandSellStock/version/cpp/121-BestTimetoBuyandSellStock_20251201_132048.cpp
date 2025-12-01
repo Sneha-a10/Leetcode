4a32529775976636d47c4a413188f27c210cff24
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2025-12-01 13:20:49 +0530

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/version/cpp/121-BestTimetoBuyandSellStock_20251201_132048.cpp
@@ -0,0 +1,15 @@
+// Last updated: 12/1/2025, 1:20:48 PM
+1class Solution {
+2public:
+3    int maxProfit(vector<int>& prices) 
+4    {
+5        int p = 0, m = prices[0];
+6        for(int v :prices)
+7        {
+8            p = max(p, v-m);
+9            m = min(v, m);
+10        }
+11        cout << INT_MAX <<" "<< INT_MIN;
+12        return p;
+13    }
+14};
