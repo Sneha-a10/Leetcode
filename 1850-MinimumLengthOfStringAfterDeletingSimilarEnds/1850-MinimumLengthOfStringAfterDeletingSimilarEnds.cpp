24b5b09a286a46eddb64b4572681e8765f5ae12e
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:56:21 +0530

    Create 1850-MinimumLengthOfStringAfterDeletingSimilarEnds

---
+++ 1850-MinimumLengthOfStringAfterDeletingSimilarEnds/1850-MinimumLengthOfStringAfterDeletingSimilarEnds.cpp
@@ -0,0 +1,18 @@
+// Last updated: 2/13/2026, 12:56:21 PM
+class Solution {
+public:
+    int minimumLength(string s) 
+    {
+        int i = 0, j = s.size()-1;
+        while(i<j && s[i] == s[j])
+        {
+            while(i<j && s[i] == s[i+1]) i++;
+            while(j>i && s[j] == s[j-1]) j--;
+            if(s[i] == s[j])
+            {
+                i++; j--;
+            }
+        }
+        return max(0,j-i+1);
+    }
+};
