f4c374c6f72409d22a8026d7d28147bb89cb6543
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:56:10 +0530

    Create 2080-CheckIfMoveIsLegal

---
+++ 2080-CheckIfMoveIsLegal/2080-CheckIfMoveIsLegal.cpp
@@ -0,0 +1,38 @@
+// Last updated: 2/13/2026, 12:56:09 PM
+class Solution {
+public:
+    bool checkMove(vector<vector<char>>& board, int rMove, int cMove, char color) 
+    {
+        char opp = (color == 'B' ? 'W' : 'B');
+        for(int i = -1; i<=1; i++)
+            for(int j = -1; j<=1; j++)
+            {
+                if(i == 0 && j == 0) continue;
+
+                int x = rMove + i;
+                int y = cMove + j;
+
+                if(x < 0 || x >= 8 || y < 0 || y >= 8) continue;
+                if(board[x][y] == color) continue;
+                if(board[x][y] != opp) continue;
+
+                x+=i;
+                y+=j;
+
+                while(x >= 0 && x < 8 && y >= 0 && y < 8)
+                {
+                    if(board[x][y] == opp)
+                    {
+                        x+=i;
+                        y+=j;
+                    }
+                    else break;
+                }
+
+                if (x >= 0 && x < 8 && y >= 0 && y < 8 && board[x][y] == color)
+ return true;
+            }
+
+        return false;    
+    }
+};
