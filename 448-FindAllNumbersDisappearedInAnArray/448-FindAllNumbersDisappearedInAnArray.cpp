22d90fef8ee165cce90d88d4bf9c947482458a99
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:57:06 +0530

    Create 448-FindAllNumbersDisappearedInAnArray

---
+++ 448-FindAllNumbersDisappearedInAnArray/448-FindAllNumbersDisappearedInAnArray.cpp
@@ -0,0 +1,21 @@
+// Last updated: 2/13/2026, 12:57:06 PM
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) 
+    {
+        vector<int> res;
+        sort(nums.begin(), nums.end());
+        int j = 1;
+
+        for(int i = 0; i<nums.size(); i++)
+        {
+            while(i<nums.size()-1 && nums[i] == nums[i+1]) i++;
+            while (nums[i] != j) res.push_back(j++);
+            if(nums[i] == j) j++;
+        }
+
+        while(j <= nums.size()) res.push_back(j++);
+
+        return res;
+    }
+};
