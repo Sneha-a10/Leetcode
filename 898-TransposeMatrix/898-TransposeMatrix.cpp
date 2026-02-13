f761afb3d3b2a60b345e5955852a35506a2c54d1
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:56:51 +0530

    Create 898-TransposeMatrix

---
+++ 898-TransposeMatrix/898-TransposeMatrix.cpp
@@ -0,0 +1,13 @@
+// Last updated: 2/13/2026, 12:56:50 PM
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) 
+    {
+        vector<vector<int>> mat (matrix[0].size(), vector<int> (matrix.size()));
+        
+        for(int i = 0; i<matrix.size(); i++)
+            for(int j = 0; j<matrix[0].size();j++)
+                mat[j][i] = matrix[i][j];
+        return mat;
+    }
+};
