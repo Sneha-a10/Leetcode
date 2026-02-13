13be7d23b5875b90a1c7bd6a723494626a10b062
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:57:20 +0530

    Create 345-ReverseVowelsOfAString

---
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString.cpp
@@ -0,0 +1,20 @@
+// Last updated: 2/13/2026, 12:57:19 PM
+class Solution {
+public:
+    string reverseVowels(string s) 
+    {
+        int l = 0, r = s.size()-1;
+        unordered_set <char> v = {'A','E','I','O','U','a','e','i','o','u'};
+        while(l<r)
+        {
+            while(l<r && v.find(s[l]) == v.end())
+            l++;
+            while(l<r && v.find(s[r]) == v.end())
+            r--;
+
+            swap(s[l], s[r]);
+            l++; r--;
+        }
+        return s;
+    }
+};
