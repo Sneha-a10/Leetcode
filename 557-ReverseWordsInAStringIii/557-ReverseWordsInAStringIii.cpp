e61594110cfdcc2ef381086a70ee2c5c2fb02770
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:57:01 +0530

    Create 557-ReverseWordsInAStringIii

---
+++ 557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.cpp
@@ -0,0 +1,25 @@
+// Last updated: 2/13/2026, 12:57:01 PM
+class Solution {
+public:
+    string reverseWords(string s) 
+    {
+        istringstream iss(s);
+        string word;
+        string ans;
+        while(iss >> word)
+        {
+            int i = 0, j = word.size()-1;
+            while(i<j)
+            {
+                swap(word[i],word[j]);
+                i++;j--;
+            }
+
+            ans += word;
+            ans += " ";
+        }
+        
+        ans.pop_back();
+        return ans;
+    }
+};
