e4cdcdbea0fd36116a07cd65b02471671e3bfb3f
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:57:52 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.cpp
@@ -0,0 +1,22 @@
+// Last updated: 2/13/2026, 12:57:52 PM
+class Solution {
+public:
+    string reverseWords(string s) 
+    {
+        istringstream iss(s);
+        vector<string> words;
+        string word;
+        while (iss >> word) 
+            words.push_back(word);
+        int i = words.size()-1;
+        string ans;
+        while(i>0)
+        {
+            ans += words[i];
+            ans += " ";
+            i --;
+        }
+        ans += words[0];
+        return ans;
+    }
+};
