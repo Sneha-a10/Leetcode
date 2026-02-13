108eda379255d08eddceb38c8348cb171edfabb7
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:57:03 +0530

    Create 556-NextGreaterElementIii

---
+++ 556-NextGreaterElementIii/556-NextGreaterElementIii.cpp
@@ -0,0 +1,25 @@
+// Last updated: 2/13/2026, 12:57:02 PM
+class Solution {
+public:
+    int nextGreaterElement(int n) 
+    {
+        string ans = to_string(n);
+        int i = ans.length()-2, j = ans.length()-1;
+        
+        while(i>=0 && ans[i]>=ans[i+1]) i--;
+        
+        if( i < 0 ) 
+            return -1;
+        
+        while(j > i-1 && ans[j]<=ans[i]) j--;
+
+        swap(ans[i],ans[j]);
+
+        reverse(ans.begin()+i+1 ,ans.end());
+
+        long res = stol(ans);
+
+        return (res <= INT_MAX) ? int(res) : -1;
+
+    }
+};
