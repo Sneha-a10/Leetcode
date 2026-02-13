d0eb8048da5dff231e855a57e99956ec3aebe2a4
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:56:52 +0530

    Create 841-ShortestDistanceToACharacter

---
+++ 841-ShortestDistanceToACharacter/841-ShortestDistanceToACharacter.cpp
@@ -0,0 +1,43 @@
+// Last updated: 2/13/2026, 12:56:52 PM
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c)
+    {
+        vector<int> ans;
+        for(int i = 0; i<s.size(); i++)
+        {
+            if(s[i] == c)
+            ans.push_back(0);
+            else
+            {
+                int j = i-1, pos1 = s.size(), pos2 = s.size();
+               
+                while(j>=0)
+                {
+                    // pos1 ++;
+                    if(s[j] == c)
+                    {
+                        pos1 = i-j;
+                        break;
+                    }
+                    j--;
+                }
+
+                j = i+1;
+
+                while(j<s.size())
+                {
+                    // pos2++;
+                    if(s[j] == c)
+                    {
+                        pos2 = j - i;
+                        break;
+                    }
+                    j++;
+                }
+                ans.push_back(min(pos1,pos2));
+            }
+        }
+        return ans;
+    }
+};
