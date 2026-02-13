007705e4b1404e23a68d63fa56d86b9b962d715c
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:57:37 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -0,0 +1,16 @@
+// Last updated: 2/13/2026, 12:57:36 PM
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        if(nums.size() == 1)
+        return false;
+        sort(nums.begin(),nums.end());
+        for(int i=1 ; i< nums.size();i++)
+        {
+            if(nums[i] == nums[i-1])
+                return true;
+        }
+        return false;
+    }
+};
