241d4ec46cd3884d20aa56094de1e24083810548
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:55:41 +0530

    Create 2787-MovementOfRobots

---
+++ 2787-MovementOfRobots/2787-MovementOfRobots.cpp
@@ -0,0 +1,27 @@
+// Last updated: 2/13/2026, 12:55:40 PM
+class Solution {
+public:
+    int sumDistance(vector<int>& nums, string s, int d) 
+    {
+        long long sum = 0;
+        long long prefix = 0;
+        int MOD = 1000000007;
+        vector<long long> final;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            if(s[i] == 'R')
+                final.push_back((long long)nums[i] + d);
+            else
+                final.push_back((long long)nums[i] - d);
+        }
+        sort(final.begin(),final.end());
+
+        for(int i = 0; i<nums.size(); i++)
+        {
+            sum = (sum + i* final[i] - prefix)%MOD;
+            prefix += final[i];
+        }
+        
+        return (sum + MOD)% MOD;
+    }
+};
