4456c0017e9b26b0747edbf2ea433b7925aa9690
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:55:54 +0530

    Create 2414-MovePiecesToObtainAString

---
+++ 2414-MovePiecesToObtainAString/2414-MovePiecesToObtainAString.cpp
@@ -0,0 +1,27 @@
+// Last updated: 2/13/2026, 12:55:54 PM
+class Solution {
+public:
+    bool canChange(string start, string target)
+    {
+        int s = 0, t = 0;
+        if(start.size()!= target.size())
+            return false;
+
+        while(s<start.size() || t<start.size())
+        {
+            while(s<start.size() && start[s]== '_') s++;
+            while(t<target.size() && target[t] == '_') t++;
+
+            if(start[s] == target[t])
+            {
+                if ((start[s] == 'L' && s<t) || (start[s] == 'R' && s>t))
+                    return false;
+            }
+            else
+                return false;
+            s++;
+            t++;
+        }
+        return true;
+    }
+};
