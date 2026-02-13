87d7b9e3a2c6142584fa8f260f00cf1cac5cd13e
Sneha Agarwal <155658764+Sneha-a10@users.noreply.github.com>
2026-02-13 12:56:56 +0530

    Create 696-CountBinarySubstrings

---
+++ 696-CountBinarySubstrings/696-CountBinarySubstrings.cpp
@@ -0,0 +1,25 @@
+// Last updated: 2/13/2026, 12:56:56 PM
+class Solution {
+public:
+    int countBinarySubstrings(string s) 
+    {
+        vector<int> ans;
+        int i = 0, j = i;
+        while(j<s.size())
+        {
+            int c = 0;
+            while(s[i]==s[j]) 
+            {
+                j++;
+                c++;
+            }
+            i = j;
+            ans.push_back(c);
+        }
+        i = 0;
+        for(int n =0; n<ans.size()-1; n++)
+            i += min(ans[n], ans[n+1]);
+        
+        return i;
+    }
+};
